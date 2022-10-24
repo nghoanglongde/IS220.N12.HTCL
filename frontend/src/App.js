2052e25dd573346d20c5f17c6d1f6ba8fb96a0e0
bnt0106 <1952222@gm.uit.edu.vn>
2022-10-24 20:20:05 +0700

    sua css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      users: []
-    }
-  }
-
-  getUsers = async () => {
-    var response = await fetch(
-      'http://localhost:5000/api/users',
-      {
-        method: 'get'
-      }
-    )
-    var responseJson = await response.json();
-    this.setState({
-      users: responseJson
-    })
-  }
 
 
   render() {
-    const users = this.state.users.map((item, index) => <li key={index}>{item.name}</li>)
     return (
       <div className="App">
-        <button onClick={this.getUsers}>Hello</button>
-        <button onClick={this.getUsers}>Chao ngocthanh</button>
-        <ul>{users}</ul>
+        <p>xin</p>
       </div>
     );
   }
---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,6 @@
+
+function Home() {
+    return <h2>home page</h2>;
+}
+
+export default Home;
