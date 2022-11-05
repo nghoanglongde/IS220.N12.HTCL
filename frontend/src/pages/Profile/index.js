ccd147bc603c94132dc3d7ecff73cbfc762e9d6f
bnt0106 <1952222@gm.uit.edu.vn>
2022-11-05 16:48:40 +0700

    sua trang profile

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -2,17 +2,20 @@ import "./Profile.css";
 import "./menuImage.css";
 import {  useEffect, useState } from "react";
 import axios from "axios";
-
+import Cookies from 'universal-cookie';
 
 
 
 function Profile() {
     const [dataImg, setDataImg] = useState([]);
     useEffect(() => {
+        const cookies = new Cookies();
+        let cookie = cookies.get('user_id');
+        console.log(cookie);
         const resData = async () => { 
             const response = await axios.post('http://localhost:5000/user/view-profile',
                 {
-                    "user_id": "634bc0f651cde90ded939af3"
+                    "user_id": cookie
                 });
             setDataImg(response.data.message)
         }
@@ -20,59 +23,6 @@ function Profile() {
     }, []);
     console.log(dataImg)
 
-
-    let data =[
-        {
-            id:1,
-            imgSrc:"https://i.pinimg.com/474x/da/67/20/da6720a11861f4270de07a2783635bd6.jpg",
-        },
-        {
-            id:2,
-            imgSrc:"https://i.pinimg.com/474x/9e/65/a3/9e65a36516af9e1a2cb731feda534bde.jpg",
-        },
-        
-
-        {
-            id:3,
-            imgSrc:"https://i.pinimg.com/474x/c1/d2/c6/c1d2c6d35b5f87810f3972e3d7bf894f.jpg",
-
-        },
-        {
-            id:4,
-            imgSrc:"https://i.pinimg.com/474x/b6/48/4c/b6484ca31c5e40a76702b191ecea2548.jpg",
-        },
-        {
-            id:5,
-            imgSrc:"https://i.pinimg.com/474x/dd/07/2c/dd072cc8ae9f37bcb17b1ce36ea83db0.jpg",
-        },
-        {
-            id:6,
-            imgSrc:"https://i.pinimg.com/474x/5c/c3/f2/5cc3f24546966bef19d45e2e56a8db2b.jpg",
-        },
-        
-
-        {
-            id:7,
-            imgSrc:"https://i.pinimg.com/474x/9c/c1/9f/9cc19fbf7300a393fa459de8de3353d2.jpg",
-
-        },
-        {
-            id:8,
-            imgSrc:"https://i.pinimg.com/474x/35/78/70/35787019bf53be64436256c8c37b874b.jpg",
-        },
-        {
-            id:9,
-            imgSrc:"https://i.pinimg.com/474x/e5/88/ed/e588edf7b1b8e9e385c27b643075b264.jpg",
-
-        },
-        {
-            id:10,
-            imgSrc:"https://i.pinimg.com/474x/a7/fe/a5/a7fea55377535abde43cea06b23d0ff7.jpg",
-        },
-        
-
-
-    ]
     const [model, setModel] = useState(false);
     const [temimgSrc, setTempImgSrc] = useState('');
     const getImg = (imgSrc) =>{
@@ -121,10 +71,11 @@ function Profile() {
                     
          </div>
          <div className="menuImage">
-            {data.map((item, index)=>{
+            {dataImg.map((item, index)=>{
+                
                 return(
-                    <div className="pics" key={index} onClick={() => getImg(item.imgSrc)}>
-                        <img src={item.imgSrc} className="imagemenu"/>
+                    <div className="pics" key={index} onClick={() => getImg(item.image)}>
+                        <img src={item.image} className="imagemenu"/>
                         
 
 
