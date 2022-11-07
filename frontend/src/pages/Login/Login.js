ec819566294e9d9e5aafd2ea107340c6e139eb67
bnt0106 <1952222@gm.uit.edu.vn>
2022-11-07 22:13:50 +0700

    code trang profile

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -40,49 +40,51 @@ function Login() {
 
     return (
     
-                <form onSubmit={handleSubmit}>
-                <h3>Sign In </h3>
-                <div className="mb-3">
-                  <label>Email address</label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Enter email"
-                    onChange={event => setAccountEmail(event.target.value)}
-                    value={accountEmail}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Enter password"
-                    onChange={event => setAccountPWD(event.target.value)}
-                    value={accountPWD}
-                  />
-                </div>
-                <div className="mb-3">
-                  <div className="custom-control custom-checkbox">
+                <div className='Applogin'>
+                  <form onSubmit={handleSubmit}>
+                  <h3>Sign In </h3>
+                  <div className="mb-3">
+                    <label>Email address</label>
                     <input
-                      type="checkbox"
-                      className="custom-control-input"
-                      id="customCheck1"
+                      type="email"
+                      className="form-control"
+                      placeholder="Enter email"
+                      onChange={event => setAccountEmail(event.target.value)}
+                      value={accountEmail}
                     />
-                    <label className="custom-control-label" htmlFor="customCheck1">
-                      Remember me
-                    </label>
                   </div>
+                  <div className="mb-3">
+                    <label>Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Enter password"
+                      onChange={event => setAccountPWD(event.target.value)}
+                      value={accountPWD}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <div className="custom-control custom-checkbox">
+                      <input
+                        type="checkbox"
+                        className="custom-control-input"
+                        id="customCheck1"
+                      />
+                      <label className="custom-control-label" htmlFor="customCheck1">
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <div className="d-grid">
+                    <button type="submit" className="submitLogin">
+                      Submit
+                    </button>
+                  </div>
+                  <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                  </p>
+                </form>
                 </div>
-                <div className="d-grid">
-                  <button type="submit" className="submitLogin">
-                    Submit
-                  </button>
-                </div>
-                <p className="forgot-password text-right">
-                  Forgot <a href="#">password?</a>
-                </p>
-              </form>
   )
 }
 
