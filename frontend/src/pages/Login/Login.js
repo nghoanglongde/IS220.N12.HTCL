1029bb84c8db1d994f2b95a8176edd257ead6d34
mythanh2001 <19522240@gm.uit.edu.vn>
2022-11-07 18:37:38 +0700

    code trang SignUp

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -3,13 +3,28 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import './Login.css';
 import {useState} from 'react';
-import Cookies from 'universal-cookie';
+import Swal from 'sweetalert2';
+
+//import Cookies from 'universal-cookie';
 
 
 
 function Login() {
     const [accountEmail, setAccountEmail] = useState('');
     const [accountPWD, setAccountPWD] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    
+    // const validation = (values) => {
+    //   let errors={};
+
+    //   if(!values.accountEmail){
+    //     errors.accountEmail="Email is required."
+    //   }
+    //   if(values.accountPWD){
+    //     errors.accountPWD="Password is required."
+    //   }
+    //   return errors;
+    // }
 
     function handleSubmit(event){
         event.preventDefault();
@@ -17,7 +32,8 @@ function Login() {
         console.log('account pwd = ', accountPWD);
         setAccountEmail('');
         setAccountPWD('');
-        
+        //setErrors(validation(values));
+
         axios.post('http://localhost:5000/user/log-in', {
                 "account_email": accountEmail,
                 "account_pwd": accountPWD
@@ -25,21 +41,44 @@ function Login() {
             .then(function (response) {
                 console.log(response);
                 if(response.data.statuscode == 200){
-                    // window.location = "/home";
-                    const cookies = new Cookies();
-                    cookies.set('user_id', response.data.message.user_id);
+                    
+                    // const cookies = new Cookies();
+                    // cookies.set('user_id', response.data.message.user_id);
+                    
+                    console.log(response.data.message.fullname);
+                    console.log(response.data.message.num_users_followed);
+                    console.log(response.data.message.num_users_following);
+                    console.log(response.data.message.about);
+                    console.log(response.data.message.avatar);
                     localStorage.setItem("fullname", response.data.message.fullname);
-                } else{
+                    localStorage.setItem("user_id", response.data.message.user_id);
+                    localStorage.setItem("num_users_followed", response.data.message.num_users_followed);
+                    localStorage.setItem("num_users_following", response.data.message.num_users_following);
+                    localStorage.setItem("about", response.data.message.about);
+                    localStorage.setItem("avatar", response.data.message.avatar);
+                    
+                    window.location = "/home";
+                }else {
+                  console.log(response)
+                  Swal.fire({
+                      text: 'Wrong email or password',
+                      icon: 'error'
+                      
+                  })
+              }
 
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+          })
+          .catch(function (error) {
+              Swal.fire({
+                  text: 'error',
+                  icon: 'error'
+              })
+            }
+          )
     }
-
     return (
     
+      
                 <form onSubmit={handleSubmit}>
                 <h3>Sign In </h3>
                 <div className="mb-3">
@@ -51,6 +90,7 @@ function Login() {
                     onChange={event => setAccountEmail(event.target.value)}
                     value={accountEmail}
                   />
+                  
                 </div>
                 <div className="mb-3">
                   <label>Password</label>
@@ -61,6 +101,7 @@ function Login() {
                     onChange={event => setAccountPWD(event.target.value)}
                     value={accountPWD}
                   />
+                  
                 </div>
                 <div className="mb-3">
                   <div className="custom-control custom-checkbox">
---
+++ frontend/src/pages/Signup/Signup.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+import './Signup.css';
+import {useState} from 'react';
+import Swal from 'sweetalert2';
+
+//import Cookies from 'universal-cookie';
+
+
+
+function Signup() {
+    const [fullname, setFullName] = useState('');
+    const [phone_number, setPhoneNumber] = useState('');
+    const [address, setAddress] = useState('');
+    const [account_email, setAccountEmail] = useState('');
+    const [account_pwd, setAccountPWD] = useState('');
+    
+    
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log('fullname = ', fullname);
+        console.log('phone_number = ', phone_number);
+        console.log('address = ', address);
+        console.log('account_email = ', account_email);
+        console.log('account_pwd = ', account_pwd);
+        setFullName('');
+        setPhoneNumber('');
+        setAddress('');
+        setAccountEmail('');
+        setAccountPWD('');
+       
+
+        axios.post('localhost:5000/user/sign-up', {
+                          "fullname": fullname,
+                          "phone_number": phone_number,
+                          "address": address,
+                          "account_email": account_email,
+                          "account_pwd": account_pwd
+            })
+            .then(function (response) {
+                console.log(response);
+                if (response.data.statuscode == 200) {
+                   window.location = "/login";
+              } else {
+                  console.log(response)
+                  Swal.fire({
+                      text: 'Account already existed',
+                      icon: 'error',
+                      confirmButtonText: 'OK'
+                  })
+              }
+
+          })
+          .catch(function (error) {
+              Swal.fire({
+                  text: 'error',
+                  icon: 'error',
+                  confirmButtonText: 'OK'
+              })
+            }
+          )
+    }
+
+    return (
+    
+      
+                <form onSubmit={handleSubmit}>
+                <h3>Sign Up </h3>
+                <div className="mb-3">
+                   <label>Full Name</label>
+                   <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter fullname"
+                      onChange={event => setFullName(event.target.value)}
+                      value={fullname}
+                  />
+                </div>
+                <div className="mb-3">
+                   <label>Phone Number</label>
+                   <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Phonenumber"
+                    onChange={event => setPhoneNumber(event.target.value)}
+                    value={phone_number}
+                  />
+                  
+                </div>
+                <div className="mb-3">
+                   <label>Address</label>
+                   <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Address"
+                    onChange={event => setAddress(event.target.value)}
+                    value={address}
+                  />
+                  
+                </div>
+                <div className="mb-3">
+                  <label>Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter address"
+                    onChange={event => setAccountEmail(event.target.value)}
+                    value={account_email}
+                  />
+                  
+                </div>
+                <div className="mb-3">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                    onChange={event => setAccountPWD(event.target.value)}
+                    value={account_pwd}
+                  />
+                  
+                </div>
+                
+                <div className="d-grid">
+                  <button type="submit" className="submitSignup">
+                    Submit
+                  </button>
+                </div>
+              </form>
+  )
+}
+
+export default Signup;
+
+
+
+
+
+
