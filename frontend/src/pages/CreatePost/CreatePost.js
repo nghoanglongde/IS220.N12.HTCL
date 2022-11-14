8ce4ab7546bfbc5cd99f2f9110b78eaefa4ce561
ngocthoai1703 <19522297@gm.uit.edu.vn>
2022-11-14 21:51:58 +0700

    'fix'

--- frontend/src/pages/CreatePost/CreatePost.js
+++ frontend/src/pages/CreatePost/CreatePost.js
@@ -6,9 +6,15 @@ import "./CreatePost.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloudUpload } from "@fortawesome/free-solid-svg-icons";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+import Swal from 'sweetalert2';
+import Cookies from 'universal-cookie';
 
 function CreatePost() {
   const [dataCategories, setDataCategories] = useState([]);
+  const [image, setImage] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDecription] = useState('');
+
   useEffect(() => {
     const resData = async () => {
       const response = await axios.get("http://localhost:5000/post/create");
@@ -18,45 +24,78 @@ function CreatePost() {
   }, []);
   console.log(dataCategories);
 
+  // let cookie = cookies.get('user_id');
+  //       console.log(cookie);
+
+  function handleImage(event){
+    event.preventDefault();
+    console.log('image = ', image);
+    console.log('title = ', title);
+    console.log('description = ', description);
+    setImage('');
+    setTitle('');
+    setDecription('');
+
+    axios.post('http://localhost:5000/post/create', {
+                          "image": image,
+                          "title": title,
+                          "description": description
+            })
+            .then(function (response) {
+              console.log(response);
+              if (response.data.statuscode == 200) {
+                 window.location = "/profile";
+            } else {
+                console.log(response)
+                Swal.fire({
+                    text: 'Error when uploading image to cloudinary',
+                    text: 'Error when insert new post to database'
+                })
+            }
+            })
+            .catch(function (error) {
+              Swal.fire({
+                  text: 'error'
+              })
+            }
+          )
+                  
+    
+  }
+ 
+
+
   return (
     <div className="form">
       <div class="wrapper">
         <div className="uploadImage">
-          {" "}
-          {/* contener*/}
           <div className="createPost">
             <label>Create Post</label>
           </div>
           <div className="frameImage">
-            {/* wrapper*/}
             <div className="imagePost">
               {/* <img src = "https://i.pinimg.com/236x/c5/bb/78/c5bb78ddfc58e8b0e97634369e4b60a0.jpg"></img> */}
             </div>
             <div className="functionPost">
-              <FontAwesomeIcon
-                icon={faCloudUpload}
-                className="iconButtonCloudUpload"
-              />
+              <FontAwesomeIcon type="file" onChange={(e) =>this.handleImage(e)} icon={faCloudUpload} className="iconButtonCloudUpload"/>
               <div className="imageText">Drop your image here or browse!</div>
             </div>
-            <div className="file-name">
-              Save and Post
-              <i />
+            <div className="file-name">File name here<i />
             </div>
           </div>
           <input id="default-btn" type="file" hidden></input>
-          <button id="custom-btn">choose a file</button>
+          <button type="submit" id="custom-btn">Save and Post</button>
         </div>
         <div className="imageContent">
           <FontAwesomeIcon icon={faEllipsisV} className="iconButtonEllipsisV" />
           <div className="addTitle">
-            <input type="text" required></input>
+            <input type="text" onChange={event => setTitle(event.target.value)} value={title} required></input>
             <div className="line"></div>
             <label>Add your title..</label>
           </div>
           <h2>Description</h2>
           <div className="contentDescription">
-            <input type="text" required></input>
+            <input type="text" onChange={event => setDecription(event.target.value)} value={description} required></input>
             <div className="lineD"></div>
             <label>Tell everyone what your description is about..</label>
           </div>
