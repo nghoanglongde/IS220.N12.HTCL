9393e0c7b04f36d259bb07dcb1a25845daa48e83
mythanh2001 <19522240@gm.uit.edu.vn>
2022-11-14 21:32:52 +0700

    css login signup

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -4,13 +4,35 @@ import axios from 'axios';
 import './Login.css';
 import { useState } from 'react';
 import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 import Cookies from 'universal-cookie';
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 
 function Login() {
     const [accountEmail, setAccountEmail] = useState('');
     const [accountPWD, setAccountPWD] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+
+    let navigate = useNavigate();
+    const routeChange = () => {
+        let path = '/signup';
+        navigate(path);
+    }
+    // const validation = (values) => {
+    //   let errors={};
+
+    //   if(!values.accountEmail){
+    //     errors.accountEmail="Email is required."
+    //   }
+    //   if(values.accountPWD){
+    //     errors.accountPWD="Password is required."
+    //   }
+    //   return errors;
+    // }
+
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -18,6 +40,7 @@ function Login() {
         console.log('account pwd = ', accountPWD);
         setAccountEmail('');
         setAccountPWD('');
+        //setErrors(validation(values));
 
         axios.post('http://localhost:5000/user/log-in', {
             "account_email": accountEmail,
@@ -29,7 +52,7 @@ function Login() {
 
                     const cookies = new Cookies();
                     cookies.set('user_id', response.data.message.user_id);
-                    
+
                     localStorage.setItem("fullname", response.data.message.fullname);
                     localStorage.setItem("phone_number", response.data.message.phone_number);
                     localStorage.setItem("address", response.data.message.address);
@@ -59,40 +82,71 @@ function Login() {
             )
     }
     return (
-        <form className='body'>
-            <form onSubmit={handleSubmit}>
-                <form className='loginform'>
-                    <h3 className="signin-heading">Sign In</h3>
-                    <label className="signin-label">Email address</label>
-                    <input
-                        type="email"
-                        className="signin-input"
-                        placeholder="Enter email"
-                        onChange={event => setAccountEmail(event.target.value)}
-                        value={accountEmail} />
-                    <label className="signin-label">Password</label>
-                    <input
-                        type="password"
-                        className="signin-input"
-                        placeholder="Enter password"
-                        onChange={event => setAccountPWD(event.target.value)}
-                        value={accountPWD} />
-                    <input
-                        type="checkbox"
-                        //className="signin-input"
-                        id="customCheck1" />
-                    <label className="check" htmlFor="customCheck1">
-                        Remember me
-                    </label>
-                    <p className="forgot-password text-right"> Forgot <a href="#">password?</a></p>
-                    <button className="submitSignup" id="sign_up_btn"> Sign up </button>
-                    <button type="submit" className="submitLogin"> Login </button>
+        <div className='body'>
+            <div className='Applogin'>
+                <form onSubmit={handleSubmit}>
+                    <div className='user'><FontAwesomeIcon icon={faUser} className='iconUser' /></div>
+                    <h3 className='signin-heading'> Sign In </h3>
+                    <div className='input-form'>
+                        <div className="signin-label">
+                            <span className='iconEmail'> <FontAwesomeIcon icon={faEnvelope} className='icon' /> </span>
+                            <label> Email address</label>
+                            <input
+                                type="email"
+                                className="signin-input"
+                                placeholder="Enter email"
+                                onChange={event => setAccountEmail(event.target.value)}
+                                value={accountEmail}
+                                required
+                            />
+                        </div>
+                        <div className="signin-label">
+                            <span className='iconLock'> <FontAwesomeIcon icon={faLock} className='icon' /> </span>
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                className="signin-input"
+                                placeholder="Enter password"
+                                onChange={event => setAccountPWD(event.target.value)}
+                                value={accountPWD}
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <div className="custom-control custom-checkbox">
+                                <input
+                                    type="checkbox"
+                                    className="custom-control-input"
+                                    id="customCheck1"
+                                />
+                                <label className="custom-control-label" htmlFor="customCheck1">
+                                    Remember me
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='btn-login'>
+                        <button type="submit" className="submitLogin">
+                            Login
+                        </button>
+                    </div>
+                    <div className='textcontent'>
+                        Or
+                    </div>
+                    <div className='btn-signup' >
+                        <button type="submit" className="submitSignup" onClick={routeChange}>
+                            Create new Account
+                        </button>
+                    </div>
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
                 </form>
-            </form>
-            <img src="https://png.pngtree.com/png-clipart/20210402/ourmid/pngtree-geometric-simple-illustration-abstract-style-png-image_3191301.jpg" class="image" alt="" />
-        </form>
+            </div>
+        </div>
 
     )
 }
 
 export default Login;
+
--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -4,6 +4,12 @@ import axios from 'axios';
 import './Signup.css';
 import { useState } from 'react';
 import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faLock, faFileText } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faAddressCard } from '@fortawesome/free-regular-svg-icons';
 
 //import Cookies from 'universal-cookie';
 
@@ -16,6 +22,11 @@ function Signup() {
     const [account_email, setAccountEmail] = useState('');
     const [account_pwd, setAccountPWD] = useState('');
 
+    let navigate = useNavigate();
+    const routeChange = () => {
+        let path = '/login';
+        navigate(path);
+    }
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -64,60 +75,94 @@ function Signup() {
 
     return (
 
-        <form className='body'>
-            <form onSubmit={handleSubmit}>
-                <form className='signupform'>
+        <div className='body'>
+            <div className='Appsignup'>
+                <form onSubmit={handleSubmit}>
+                <div className='info'><FontAwesomeIcon icon={faAddressCard} className='iconInfo' /></div>
                     <h3 className="signup-heading">Sign Up </h3>
-
-                    <label className="signup-label">Full Name</label>
-                    <input
-                        type="text"
-                        className="signup-input"
-                        placeholder="Enter fullname"
-                        onChange={event => setFullName(event.target.value)}
-                        value={fullname}
-                        required
-                    />
-                    <label className="signup-label">Phone Number</label>
-                    <input
-                        type="text"
-                        className="signup-input"
-                        placeholder="Enter Phonenumber"
-                        onChange={event => setPhoneNumber(event.target.value)}
-                        value={phone_number}
-                    />
-                    <label className="signup-label">Address</label>
-                    <input
-                        type="text"
-                        className="signup-input"
-                        placeholder="Enter Address"
-                        onChange={event => setAddress(event.target.value)}
-                        value={address}
-
-                    />
-                    <label className="signup-label">Email address</label>
-                    <input
-                        type="email"
-                        className="signup-input"
-                        placeholder="Enter Email"
-                        onChange={event => setAccountEmail(event.target.value)}
-                        value={account_email}
-                        required
-                    />
-                    <label className="signup-label">Password</label>
-                    <input
-                        type="password"
-                        className="signup-input"
-                        placeholder="Enter password"
-                        onChange={event => setAccountPWD(event.target.value)}
-                        value={account_pwd}
-                        required
-                    />
-                    <button type="submit" className="submitLogin"> Sign in </button>
+                    <div className='signup-form'>
+                    <div className="signup-label">
+                        <span className='iconPhone'> <FontAwesomeIcon icon={faFileText} className='icon' /> </span>
+                        <label>Full Name</label>
+                        <input
+                            type="text"
+                            className="signup-input"
+                            placeholder="Enter fullname"
+                            onChange={event => setFullName(event.target.value)}
+                            value={fullname}
+                            required
+                        />
+                    </div>
+                    <div className='signup-label'>
+                        <span className='iconPhone'> <FontAwesomeIcon icon={faPhone} className='icon' /> </span>
+                        <label> Phone Number</label>
+                        <input
+                            type="text"
+                            className="signup-input"
+                            placeholder="Enter Phonenumber"
+                            onChange={event => setPhoneNumber(event.target.value)}
+                            value={phone_number}
+                        />
+                    </div>
+                    <div className="signup-label">
+                    <span className='iconLocation'> <FontAwesomeIcon icon={faLocationDot} className='icon' /> </span>
+                        <label >Address</label>
+                        <input
+                            type="text"
+                            className="signup-input"
+                            placeholder="Enter Address"
+                            onChange={event => setAddress(event.target.value)}
+                            value={address}
+                        />
+                    </div>
+                    <div className="signup-label">
+                        <span className='iconEmail'> <FontAwesomeIcon icon={faEnvelope} className='icon' /> </span>
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="signup-input"
+                            placeholder="Enter Email"
+                            onChange={event => setAccountEmail(event.target.value)}
+                            value={account_email}
+                            required
+                        />
+                    </div>
+                    <div className="signup-label">
+                        <span className='iconLock'> <FontAwesomeIcon icon={faLock} className='icon' /> </span>
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="signup-input"
+                            placeholder="Enter password"
+                            onChange={event => setAccountPWD(event.target.value)}
+                            value={account_pwd}
+                            required
+                        />
+                    </div>
+                    </div>
+                    <div className="mb-4">
+                            <div className="custom-control custom-checkbox">
+                                <input
+                                    type="checkbox"
+                                    className="custom-control-input"
+                                    id="customCheck1"
+                                    required
+                                />
+                                <label className="custom-control-label text-right" htmlFor="customCheck1">
+                                I accept the terms and conditions
+                                </label>
+                            </div>
+                        </div>
+                    <div className='btn-'>
                     <button className="submitSignup" id="sign_up_btn"> Sign up </button>
+                    <div className='textcontent'>
+                        Or
+                    </div>
+                    <button type="submit" className="submitLogin" onClick={routeChange}> Sign in </button>
+                    </div>
                 </form>
-            </form>
-        </form>
+            </div>
+        </div>
     )
 }
 
